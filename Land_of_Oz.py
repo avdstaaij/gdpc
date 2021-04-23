dddf2693f4a11d7112760d3d5888185cf61bf3eb
Flashing-Blinkenlights <blinkenlights@pm.me>
2021-04-23 17:30:14 +0200

    Made Land_of_Oz mark all chunks containing water biomes

--- Land_of_Oz.py
+++ Land_of_Oz.py
@@ -18,7 +18,7 @@ This file is not meant to be imported.
 __all__ = []
 __author__ = "Blinkenlights"
 __version__ = "v4.2_dev"
-__date__ = "22 April 2021"
+__date__ = "23 April 2021"
 
 from random import choice, randint
 
@@ -27,18 +27,50 @@ import numpy as np
 import worldLoader as WL
 
 # custom default build area with override
-STARTX, STARTY, STARTZ, ENDX, ENDY, ENDZ = 0, 0, 0, 255, 255, 255
+STARTX, STARTY, STARTZ, ENDX, ENDY, ENDZ = 0, 0, 0, 999, 255, 999
 if IU.requestBuildArea() != [0, 0, 0, 127, 255, 127]:
     STARTX, STARTY, STARTZ, ENDX, ENDY, ENDZ = IU.requestBuildArea()
 
-WORLDSLICE = WL.WorldSlice(STARTX, ENDX, STARTZ, ENDZ)
+WORLDSLICE = WL.WorldSlice(STARTX, STARTZ, ENDX, ENDZ)
 XCHUNKSPAN, ZCHUNKSPAN = WORLDSLICE.chunkRect[2], WORLDSLICE.chunkRect[3]
 
 POLITICAL_REGIONS = np.array((XCHUNKSPAN, ZCHUNKSPAN))
 
+CITY_SIZE = 128
+
 
 def analyzeChunks():
     """Analyze chunks in the build area to determine geographic layout."""
+    # replace with getBiomesAt
+
+    IU.setBuffering(True)
+    IU.setBufferLimit(4096)
+
+    IU.fill(STARTX, 255, STARTZ, ENDX, 255, ENDZ, "air")
+    IU.sendBlocks()
+
+    for x in range(WORLDSLICE.chunkRect[2]):
+        for z in range(WORLDSLICE.chunkRect[3]):
+            chunkID = x + z * WORLDSLICE.chunkRect[2]
+            biomes = WORLDSLICE.nbtfile['Chunks'][chunkID]['Level']['Biomes']
+            biomes = list(set(biomes))
+            print(biomes)
+            if (0 in biomes or 7 in biomes or 24 in biomes or 44 in biomes
+                    or 45 in biomes or 46 in biomes or 47 in biomes
+                    or 48 in biomes or 49 in biomes or 50 in biomes):
+                xstart = WORLDSLICE.nbtfile['Chunks'][chunkID]['Level']['xPos'].value * 16
+                xend = xstart + 15
+                zstart = WORLDSLICE.nbtfile['Chunks'][chunkID]['Level']['zPos'].value * 16
+                zend = zstart + 15
+                print(f"filling {xstart, zstart, xend, zend}")
+                IU.fill(xstart, 255, zstart, xend,
+                        255, zend, "snow_block")
+                IU.sendBlocks()
+                print("filling done.")
+
+
+def buildCity():
+    """Build the Emerald City and set the spawn there."""
 
 
 if __name__ == '__main__':
