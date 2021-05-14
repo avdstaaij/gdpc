dcb4f04ef4fedd8f70786836ce14dd1a01d128b3
Flashing-Blinkenlights <blinkenlights@pm.me>
2021-05-14 09:15:48 +0200

    Cleaned up bitarray; added `__repr__` to BitArray class

--- bitarray.py
+++ bitarray.py
@@ -1,43 +1,55 @@
 #! /usr/bin/python3
-"""### Read the bitarray format used by Minecraft.
-"""
+"""### Read the bitarray format used by Minecraft."""
 __all__ = ['BitArray']
-
-# __version__
+__version__ = 'v4.2_dev'
 
 from math import floor
 
 
 def inclusiveBetween(start, end, value):
-    if value < start or value > end:
+    """**Raise an exception when the value is out of bounds**."""
+    if start > value > end:
         raise ValueError(
-            f"The value {value} is not in the inclusive range of {start} to {end}")
+            f"The value {value} is not in the inclusive range "
+            f"of {start} to {end}")
 
-
-# Minecraft stores block and heightmap data in compacted arrays of longs. This class does the proper index mapping
-# and bit shifting to get to the actual data.
 
 class BitArray:
+    """**Store an array of binary values and its metrics**.
+
+    Minecraft stores block and heightmap data in compacted arrays of longs.
+    This class performs index mapping and bit shifting to access the data.
+    """
+
     def __init__(self, bitsPerEntryIn, arraySizeIn, data):
+        """**Initialise a BitArray**."""
         inclusiveBetween(1, 32, bitsPerEntryIn)
         self.arraySize = arraySizeIn
         self.bitsPerEntry = bitsPerEntryIn
         self.maxEntryValue = (1 << bitsPerEntryIn) - 1
         self.entriesPerLong = floor(64 / bitsPerEntryIn)
-        j = floor((arraySizeIn + self.entriesPerLong - 1) / self.entriesPerLong)
+        j = floor((arraySizeIn + self.entriesPerLong - 1)
+                  / self.entriesPerLong)
         if data is not None:
             if len(data) != j:
                 raise Exception(
-                    f"Invalid length given for storage, got: {len(data)} but expected: {j}")
+                    "Invalid length given for storage, "
+                    f"got: {len(data)} but expected: {j}")
 
             self.longArray = data
         else:
             self.longArray = []  # length j
 
+    def __repr__(self):
+        """**Represent the stored data**."""
+        return str(self.longArray)
+
     def getPosOfLong(self, index):
-        return int(index / self.entriesPerLong)
+        """**Return the position of the long that contains index**."""
+        return index // self.entriesPerLong
 
     def getAt(self, index):
+        """**Return the binary value stored at index.**."""
         inclusiveBetween(0, (self.arraySize - 1), index)
         i = self.getPosOfLong(index)
         # print("{} > {}".format(index, i))
@@ -46,4 +58,5 @@ class BitArray:
         return j >> k & self.maxEntryValue
 
     def size(self):
+        """**Return self.arraySize**."""
         return self.arraySize
